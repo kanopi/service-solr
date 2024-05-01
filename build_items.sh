ca537e90518e5f358bb5e6f776846ff767503412
Sean Dietrich <sean@kanopi.com>
2024-04-30 22:45:14 -0700

    Updated build items script

--- build_items.sh
+++ build_items.sh
@@ -7,7 +7,7 @@ if [[ -n "${DEBUG}" ]]; then
 fi
 
 REPO=devkteam/solr
-SOLR_DEFAULT_CONFIG_SET="search_api_solr_4.3.0"
+SOLR_DEFAULT_CONFIG_SET="search_api_solr_4.3.3"
 
 build() {
     git checkout -- configsets
@@ -28,7 +28,9 @@ build() {
 
 TAG=8,8.11,8.11.2 VERSION=8.11 FROM=solr:8.11.2-slim build
 TAG=8-extras,8.11-extras,8.11.2-extras VERSION=8.11-extras FROM=solr:8.11.2-slim INCLUDE_EXTRAS=1 build
-TAG=9.3,9.3.0 VERSION=9.3 FROM=solr:9.3.0-slim build
-TAG=9.3-extras,9.3.0-extras VERSION=9.3-extras FROM=solr:9.3.0-slim INCLUDE_EXTRAS=1 build
-TAG=9,9.4,9.4.0 VERSION=9.4 FROM=solr:9.4.0-slim build
-TAG=9-extras,9.4-extras,9.4.0-extras VERSION=9.4-extras FROM=solr:9.4.0-slim INCLUDE_EXTRAS=1 build
+#TAG=9.3,9.3.0 VERSION=9.3 FROM=solr:9.3.0-slim build
+#TAG=9.3-extras,9.3.0-extras VERSION=9.3-extras FROM=solr:9.3.0-slim INCLUDE_EXTRAS=1 build
+TAG=9.4,9.4.0 VERSION=9.4 FROM=solr:9.4.0-slim build
+TAG=9.4-extras,9.4.0-extras VERSION=9.4-extras FROM=solr:9.4.0-slim INCLUDE_EXTRAS=1 build
+TAG=9,9.5,9.5.0 VERSION=9.5 FROM=solr:9.5.0-slim build
+TAG=9-extras,9.5-extras,9.5.0-extras VERSION=9.5-extras FROM=solr:9.5.0-slim INCLUDE_EXTRAS=1 build
